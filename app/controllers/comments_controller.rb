093721b60828bfef5e7e8a0b600e853ba7ccba5c
k-mstn <knt.masutani@gmail.com>
2023-03-09 15:04:20 +0900

    third commit

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,6 +10,8 @@ class CommentsController < ApplicationController
     end
   end
 
+  private
+
   def comment_params
     params.require(:comment).permit(:content).merge(user_id: current_user.id, prototype_id: params[:prototype_id])
   end
